42967a7a30dd1f8d7714234a88c7ca8c53628559
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2021-06-25 18:10:47 +0800

    Co-authored-by: ZEQUANR <ZEQUANR@users.noreply.github.com>

--- dashboard.js
+++ dashboard.js
@@ -1,261 +1,342 @@
 const body = document.body
 
+const titleStyle = {
+  color: "#fff",
+  fontStyle: "normal",
+  fontWeight: "normal",
+  fontFamily: "sans-serif",
+  fontSize: 22,
+  lineHeight: "38",
+  textBorderColor: "transparent",
+  textBorderWidth: 1,
+  textShadowColor: "black",
+  textShadowBlur: 3,
+  textShadowOffsetX: 3,
+  textShadowOffsetY: 2,
+}
+
 const fontStyle = {
-    color: "#fff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    fontFamily: "sans-serif",
-    fontSize: 18,
-    lineHeight: "30",
-    textBorderColor: "transparent",
-    textBorderWidth: 1,
-    textShadowColor: "black",
-    textShadowBlur: 3,
-    textShadowOffsetX: 3,
-    textShadowOffsetY: 2,
+  color: "#fff",
+  fontStyle: "normal",
+  fontWeight: "normal",
+  fontFamily: "sans-serif",
+  fontSize: 12,
+  lineHeight: "38",
+  textBorderColor: "transparent",
+  textBorderWidth: 1,
+  textShadowColor: "black",
+  textShadowBlur: 3,
+  textShadowOffsetX: 3,
+  textShadowOffsetY: 2,
 }
 
-function carateBarCharts(charName, node) {
-    let div = document.createElement('div')
-    div.id = charName
-    div.style = 'width: 100%;height:100%;'
-    document.querySelector('#' + node).appendChild(div)
-    let chart = document.querySelector("#" + charName)
-    let myChart = echarts.init(chart)
-    let option = {
-        color: ["#FF9900"],
-        title: {
-            text: 'ECharts ',
-            textStyle: {
-                fontStyle
-            },
+function carateBarCharts(chartsName, node, data, title, yName) {
+  let div = document.createElement('div')
+  div.id = chartsName
+  div.style = 'width: 100%;height:100%;'
+  document.querySelector('#' + node).appendChild(div)
+  const chart = document.querySelector("#" + chartsName)
+  const myChart = echarts.init(chart)
+  const option = {
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '0%',
+      containLabel: true
+    },
+    title: {
+      text: title,
+      textStyle: titleStyle,
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      },
+    },
+    legend: {
+      data: ['销量'],
+      right: '2%',
+      top: '4%',
+      textStyle: fontStyle,
+    },
+    xAxis: {
+      data: data.map(item => item.name),
+      axisLabel: {
+        color: '#000',
+        interval: 0,
+        formatter: function (value) {
+          return value.split("").join("\n");
         },
-        tooltip: {},
-        legend: {
-            data: ['销量'],
-            right: '2%',
-            top: '4%',
-            textStyle: {
-                color: "#fff",
-                fontStyle: "normal",
-                fontWeight: "normal",
-                fontFamily: "sans-serif",
-                fontSize: 14,
-                lineHeight: "30",
-                textBorderColor: "transparent",
-                textBorderWidth: 1,
-                textShadowColor: "black",
-                textShadowBlur: 3,
-                textShadowOffsetX: 3,
-                textShadowOffsetY: 2,
-            },
-        },
-        xAxis: {
-            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
-    };
-    myChart.setOption(option);
+        textStyle: {
+          fontSize: 8,
+          color: '#fff',
+        }
+      },
+    },
+    yAxis: {},
+    series: [{
+      name: yName,
+      type: 'bar',
+      data: data.map(item => item.value),
+    }]
+  };
+  myChart.setOption(option);
 }
 
-function createRight2(charName, node, data) {
-    let div = document.createElement('div')
-    div.id = charName
-    div.style = 'width: 100%;height:100%;'
-    document.querySelector('#' + node).appendChild(div)
+function createRingCharts(chartsName, node, data, title) {
+  let div = document.createElement('div')
+  div.id = chartsName
+  div.style = 'width: 100%;height:100%;'
+  document.querySelector('#' + node).appendChild(div)
+  const chart = document.querySelector("#" + chartsName)
+  const myChart = echarts.init(chart)
+  const option = {
+    title: {
+      text: title,
+      textStyle: titleStyle,
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        restore: { show: true },
+        saveAsImage: { show: true }
+      }
+    },
+    series: [{
+      name: "装机占比",
+      type: 'pie',
+      radius: [20, 86],
+      center: ['50%', '60%'],
+      roseType: 'area',
+      itemStyle: {
+        borderRadius: 4
+      },
+      data: data.map(item => { return { name: item.name, value: item.value } }),
+    }]
+  };
+  myChart.setOption(option);
+}
 
-    const line = document.getElementById(charName)
-    const myCharts = echarts.init(line)
-    const option = {
-        title: {
-            text: "各省新能源汽车保有量",
-            subtext: "单位（万辆）",
-            textStyle: {
-                color: "#fff",
-                fontStyle: "normal",
-                fontWeight: "normal",
-                fontFamily: "sans-serif",
-                fontSize: 18,
-                lineHeight: "30",
-                textBorderColor: "transparent",
-                textBorderWidth: 1,
-                textShadowColor: "black",
-                textShadowBlur: 3,
-                textShadowOffsetX: 3,
-                textShadowOffsetY: 2,
-            },
-            subtextStyle: {
-                fontSize: 15
-            }
-        },
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'shadow'
-            },
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: [{
-            type: 'category',
-            axisLine: {
-                show: false
-            },
-            data: data.map(item => item.name),
-            axisTick: {
-                alignWithLabel: true
-            }
-        }],
-        yAxis: [{
-            type: 'value',
-            show: false,
-        }],
-        series: [{
-            name: '保有量',
-            type: 'bar',
-            barWidth: '60%',
-            data: data.map(item => item.car)
-        }]
-    };
-    myCharts.setOption(option)
+function carateLineCharts(chartsName, node, data, title, yName) {
+  let div = document.createElement('div')
+  div.id = chartsName
+  div.style = 'width: 100%;height:100%;'
+  document.querySelector('#' + node).appendChild(div)
+  const chart = document.querySelector("#" + chartsName)
+  const myChart = echarts.init(chart)
+  option = {
+    title: {
+      text: title,
+      textStyle: titleStyle,
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    grid: {
+      top: 10
+    },
+    legend: {
+      data: ['USA', 'CN', 'EU'],
+      top: '15%',
+      textStyle: fontStyle,
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: data.map(item => item.year)
+    },
+    yAxis: {
+      type: 'value',
+      splitLine: {
+        show: false
+      }
+    },
+    series: [
+      {
+        name: 'USA',
+        type: 'line',
+        data: data.map(item => item.USA)
+      },
+      {
+        name: 'CN',
+        type: 'line',
+        data: data.map(item => item.CN)
+      },
+      {
+        name: 'EU',
+        type: 'line',
+        data: data.map(item => item.EU)
+      }
+    ]
+  };
+  myChart.setOption(option);
 }
 
 function createMap(charName, node) {
-    let div = document.createElement('div')
-    div.id = charName
-    div.style = 'width: 100%;height:100%;'
-    document.querySelector('#' + node).appendChild(div)
+  let div = document.createElement('div')
+  div.id = charName
+  div.style = 'width: 100%;height:100%;'
+  document.querySelector('#' + node).appendChild(div)
 
-    const myChart = echarts.init(document.getElementById(charName))
-
-    echarts.registerMap('CHA', china)
-
-    const option = {
-        title: {
-            text: "各省新能源汽车保有量",
-            left: "center",
-            textStyle: {
-                //   fontSize: 30,
-                color: "black"
-            },
+  const myChart = echarts.init(document.getElementById(charName))
+  
+  echarts.registerMap('china', geoJson);
+  
+  const option = {
+    title: {
+      text: "各省新能源汽车保有量",
+      left: "center",
+      textStyle: titleStyle,
+    },
+    
+    tooltip: {
+      trigger: 'item'
+    },
+    
+    geo: {
+      map: 'china',
+      zoom: 1.2,
+      aspectScale: 0.75,
+      itemStyle: {
+        shadowColor:'rgb(58,115,192)',
+        shadowOffsetX: 10,
+        shadowOffsetY: 10,
+      },
+      regions: [{
+        name: '南海诸岛',
+        itemStyle: {
+          opacity: 0
+        }
+      }]
+    },
+    
+    visualMap: {
+      min: 0,
+      max: 90,
+      left: 'left',
+      text: ['High', 'Low'],
+      textStyle: {
+        color: 'white'
+      }, 
+      realtime: true,
+      calculable: false,
+      inRange: {
+        color: ['#72C6EF', '#004E8F']
+      }
+    },
+    
+    series: [{
+      name: '单位（万辆）',
+      type: 'map',
+      zoom: 1.2,
+      map: 'china',
+      label: {
+        show: false
+      },
+      data: newCar.map(item => {
+        return  {name: item.name, value: item.value};
+      }),
+      itemStyle: {
+        normal: {
+          borderColor: 'rgb(147, 235, 248)',
+          borderWidth: 1,
         },
-
-        tooltip: {
-            trigger: 'item'
-        },
-
-        visualMap: {
-            min: 0,
-            max: 90,
-            left: 'left',
-            text: ['High', 'Low'],
-            realtime: true,
-            calculable: true,
-            inRange: {
-                color: ['#72C6EF', '#004E8F']
-            }
-        },
-
-        series: [{
-            name: '单位（万辆）',
-            type: 'map',
-            zoom: 1.2,
-            mapType: 'CHA',
-            label: {
-                show: false
-            },
-            data: newCar.map(item => {
-                var rObj = {};
-                rObj.name = item.name
-                rObj.value = item.car
-                return rObj
-            })
-        }]
-    };
-    myChart.setOption(option);
+        emphasis: {
+          areaColor: 'rgb(46,229,206)',
+          borderWidth: 0.1
+        }
+      }
+    }]
+  };
+  myChart.setOption(option);
 }
 
-
 function createRight0(charName, node) {
-    let div = document.createElement('div')
-    div.id = charName
-    div.style = 'width: 100%;height:100%;'
-    document.querySelector('#' + node).appendChild(div)
+  let div = document.createElement('div')
+  div.id = charName
+  div.style = 'width: 100%;height:100%;'
+  document.querySelector('#' + node).appendChild(div)
 
-    const myCharts = echarts.init(document.getElementById(charName))
+  const myCharts = echarts.init(document.getElementById(charName))
 
-    const option = {
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'shadow'
-            }
-        },
-        legend: {
-            data: ['Direct', 'Mail Ad', 'Klooo']
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: {
-            type: 'value',
-            show: false,
-            boundaryGap: ['0%', '-%']
-        },
-        yAxis: {
-            type: 'category',
-            show: false,
-            data: ['Tue']
-        },
-        series: [{
-                name: 'Direct',
-                type: 'bar',
-                stack: 'total',
-                label: {
-                    show: true
-                },
-                emphasis: {
-                    focus: 'series'
-                },
-                data: [10]
-            },
-            {
-                name: 'Mail Ad',
-                type: 'bar',
-                stack: 'total',
-                label: {
-                    show: true
-                },
-                emphasis: {
-                    focus: 'series'
-                },
-                data: [150]
-            },
-            {
-                name: 'Klooo',
-                type: 'bar',
-                stack: 'total',
-                label: {
-                    show: true
-                },
-                emphasis: {
-                    focus: 'series'
-                },
-                data: [150]
-            }
-        ]
-    };
-    myCharts.setOption(option);
+  const option = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {
+      data: ['Direct', 'Mail Ad', 'Klooo'],
+      textStyle: fontStyle,
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: {
+      type: 'value',
+      show: false,
+      boundaryGap: ['0%', '-%']
+    },
+    yAxis: {
+      type: 'category',
+      show: false,
+      data: ['Tue']
+    },
+    series: [{
+      name: 'Direct',
+      type: 'bar',
+      stack: 'total',
+      label: {
+        show: true
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [10]
+    },
+    {
+      name: 'Mail Ad',
+      type: 'bar',
+      stack: 'total',
+      label: {
+        show: true
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [150]
+    },
+    {
+      name: 'Klooo',
+      type: 'bar',
+      stack: 'total',
+      label: {
+        show: true
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [150]
+    }
+    ]
+  };
+  myCharts.setOption(option);
 }
---
+++ data/battery.js
@@ -0,0 +1,8 @@
+const battery = [
+  {"name": '宁德时代', "value": 0.25},
+  {"name": 'LG 化学', "value": 0.23},
+  {"name": '松下', "value": 0.18},
+  {"name": '比亚迪', "value": 0.07},
+  {"name": '三星 SDI', "value": 0.06},
+  {"name": '其它', "value":0.21},
+]
